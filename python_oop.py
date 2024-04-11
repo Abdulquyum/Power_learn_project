737e24c1bd1fe6e5703c5e1f01ece6d7591cccdf
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-12 00:36:46 +0100

    Class Person

---
+++ python_oop.py
@@ -0,0 +1,11 @@
+class Person():
+    def __init__(self):
+        seff.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(name, age, gender):
+        print(f"Hi, My name is {name}, I'm {age} years old and I am a {gender}")
+
+
+Person.introduce("Abdulquyum", 10, "Male")
